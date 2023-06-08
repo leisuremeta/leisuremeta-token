bbff2415c9bb763819302a8fe71d60025192e469
sungkmi <sungkmi@gmail.com>
2023-06-08 15:26:42 +0900

    change initial d-day to 1677120000 (2023-02-23T02:40:00Z)

--- migrations/2_deploy_leisuremeta.js
+++ migrations/2_deploy_leisuremeta.js
@@ -1,7 +1,7 @@
 const LM = artifacts.require('LeisureMeta');
 const fs = require('fs');
 const daopoolAddress = fs.readFileSync(`${__dirname}/../.secret.daopool`).toString().trim();
-const initialDDay = 1798761600; // 2027-01-01 00:00:00 GMT
+const initialDDay = 1677120000; // 2023-02-23 02:40:00 GMT
 
 
 module.exports = async function (deployer) {
